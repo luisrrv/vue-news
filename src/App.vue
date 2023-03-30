fa4fccacc637cfb8e7a0e474b94cd0c1e9851168
rodluis <luis.rodriguez@evolany.com>
2023-03-30 18:07:57 +0900

    first commit, understanding Vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <NavBar string="nav text" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  },
+  created(){
+    console.log('APP');
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    props: {
+        msg: String,
+        string: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            default: 20,
+        }
+    },
+    data(){
+        return {
+            greeting: 'Hello!',
+        }
+    },
+    created() {  
+        console.log(this.greeting);
+        console.log(this.string);
+        console.log(this.age);
+    }
+}
+</script>
