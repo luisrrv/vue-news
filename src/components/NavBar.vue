fff5e77c71b10ae0a549be64f05f01484d0534d5
rodluis <luis.rodriguez@evolany.com>
2023-03-31 17:55:29 +0900

    basic counter for learning

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>{{ greeting }}</h1>
+    <h3 class="number">{{ count }}</h3>
+    <button class="btn" @click="decCount">-</button>
+    <button class="btn" @click="incCount">+</button>
 </template>
 
 <script>
@@ -11,20 +14,47 @@ export default {
             type: String,
             required: true
         },
-        age: {
+        counter: {
             type: Number,
-            default: 20,
+            default: 0,
         }
     },
     data(){
         return {
             greeting: 'Hello!',
+            count: 0,
         }
     },
     created() {  
         console.log(this.greeting);
         console.log(this.string);
-        console.log(this.age);
+        console.log('count:',this.count);
+    },
+    methods: {
+        incCount() {
+            this.count++;
+        },
+        decCount() {
+            this.count--;
+        }
     }
 }
 </script>
+
+<style>
+    .btn {
+        margin: 8px;
+        padding: 8px 16px;
+        border: none;
+        background-color: black;
+        color: white;
+        border-radius: 12px;
+        font-size: 20px;
+    }
+    .btn:hover {
+        cursor: pointer;
+    }
+    .number {
+        font-size: 60px;
+    }
+</style>
